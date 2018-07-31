c0d280326f0de744c35ba96e5d387f9cedc599ba
Giovanni <gpetruc@gmail.com>
2018-07-31 13:58:01 +0200

    Add pre-computed relIso with PU corrections (simplify cut files)

--- TTHAnalysis/python/analyzers/treeProducerXtracks.py
+++ TTHAnalysis/python/analyzers/treeProducerXtracks.py
@@ -59,6 +59,9 @@ metTypeXtracksBasic = NTupleObjectType("metXtracksBasic", baseObjectTypes = [ fo
 ## IsoTrackDeDx
 ##------------------------------------------
 
+def _isoDBeta(x):
+    return x.chargedHadronIso() + max(x.neutralHadronIso()+x.photonIso()-0.5*x.puChargedHadronIso(), 0)
+
 isoTrackTypeDeDx = NTupleObjectType("isoTrackTypeDeDx", baseObjectTypes = [ particleType ], variables = [
     NTupleVariable("charge",   lambda x : x.charge(), int),
     NTupleVariable("dxy",   lambda x : x.dxy(), help="d_{xy} with respect to PV, in cm (with sign)"),
@@ -84,10 +87,12 @@ isoTrackTypeDeDx = NTupleObjectType("isoTrackTypeDeDx", baseObjectTypes = [ part
     NTupleVariable("miniIsoNH",   lambda x : x.miniPFIsolation().neutralHadronIso(), help="Neutral hadron mini-isolation"),
     NTupleVariable("miniIsoPH",   lambda x : x.miniPFIsolation().photonIso(), help="Photon mini-isolation"),
     NTupleVariable("miniIsoPU",   lambda x : x.miniPFIsolation().puChargedHadronIso(), help="Pileup charged hadron mini-isolation"),
+    NTupleVariable("miniRelIso",   lambda x : _isoDBeta(x.miniPFIsolation())/x.pt(), help="mini-isolation (relative, PU-corrected using dBeta)"),
     NTupleVariable("dR03IsoCH",   lambda x : x.pfIsolationDR03().chargedHadronIso(), help="Charged hadron isolation dR=0.3"),
     NTupleVariable("dR03IsoNH",   lambda x : x.pfIsolationDR03().neutralHadronIso(), help="Neutral hadron isolation dR=0.3"),
     NTupleVariable("dR03IsoPH",   lambda x : x.pfIsolationDR03().photonIso(), help="Photon isolation = dR=0.3"),
     NTupleVariable("dR03IsoPU",   lambda x : x.pfIsolationDR03().puChargedHadronIso(), help="Pileup charged hadron isolation dR=0.3"),
+    NTupleVariable("relIso03",   lambda x : _isoDBeta(x.pfIsolationDR03())/x.pt(), help="relative isolation dR=0.3 (PU-corrected using dBeta)"),
     NTupleVariable("caloEmEnergy",   lambda x : x.matchedCaloJetEmEnergy(), help="Energy in the ECAL behind the track"),
     NTupleVariable("caloHadEnergy",   lambda x : x.matchedCaloJetHadEnergy(), help="Energy in the HCAL behind the track"),
     NTupleVariable("channelsGoodECAL", lambda x : x.channelsGoodECAL, int, help="Flag set to 1 when the track extrapolates to all good ECAL channels"),
